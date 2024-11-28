0fea90488e182ad69211941573617e74f2230650
Ane <a_izaguirrelor@fpzornotza.com>
2024-11-28 10:05:11 +0100

    commit Ane

--- PullRequestPruebaEquipo/src/base/Principal.java
+++ PullRequestPruebaEquipo/src/base/Principal.java
@@ -1,10 +1,19 @@
 package base;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello World");
+		
+		String izena;
+		
+		System.out.println("Zein da zure izena");
+		izena=sc.next();
+		System.out.println(izena);
+		sc.close();
 		
 	}
 
