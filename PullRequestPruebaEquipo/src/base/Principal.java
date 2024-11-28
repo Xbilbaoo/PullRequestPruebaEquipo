075b639c34613987b3da6d79a64c1ff563b67d41
Xabier <x_bilbaozam@fpzornotza.com>
2024-11-28 10:12:23 +0100

    minor changes.

--- PullRequestPruebaEquipo/src/base/Principal.java
+++ PullRequestPruebaEquipo/src/base/Principal.java
@@ -1,11 +1,30 @@
 package base;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
+		// Aldagaiak
+		
+		Scanner sc = new Scanner(System.in);
+		String izena;
+		
+		
+		
+		
+		// Programa
+		
 		System.out.println("Hello World");
 		
+		System.out.println("Javier Vilvado");
+		
+		System.out.println("Zein da zure izena?");
+		izena=sc.next();
+		
+		System.out.println(izena);
+		sc.close();
 	}
 
 }
